f2570530593564ad5a144471dea77ef8d7fa10bd
Victor Andrade <victor.w.andrade@gmail.com>
2024-03-06 10:44:36 -0300

    Update and rename Day 2 - Understanding Data Types and How to Manipulate Strings - revisited.py to TipCalculator as a Function.py

--- Basic_Projects_HowIStarted/TipCalculator as a Function.py
+++ Basic_Projects_HowIStarted/TipCalculator as a Function.py
@@ -1,9 +1,10 @@
-def tip_calculator():
+def tip_calculator(percentage):
     total = int(input("how much was the total bill? "))
     people = int(input("how many people are splitting the bill? "))
     percentage = int(input("whats the percentage you want to give? 10, 12, 15, 20? "))
     
     total_with_tip = total * (1 + percentage/100)
     print (f"Each person should pay: {total_with_tip/people}")
-    
-tip_calculator()
+
+tip_percentage = int(input("whats the percentage you want to tip? enter as a whole number (Example: 10 for 10%): "))
+tip_calculator(tip_percentage)
