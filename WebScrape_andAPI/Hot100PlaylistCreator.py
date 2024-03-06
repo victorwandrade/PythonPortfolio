e82295aa343a5b191bba482a058a43b44d745a8f
Victor Andrade <victor.w.andrade@gmail.com>
2024-03-06 10:18:29 -0300

    Update and rename main.py to WebScrape_andAPI/Hot100PlaylistCreator.py
    Webscrape billboard hot 100 list and then create a playlist on spotify through its API

--- WebScrape_andAPI/Hot100PlaylistCreator.py
+++ WebScrape_andAPI/Hot100PlaylistCreator.py
@@ -4,16 +4,15 @@ import spotipy
 from spotipy.oauth2 import SpotifyOAuth
 import pprint
 
-TIMETRAVEL = input("which year do you want to travel to? Type in this format: YYYY-MM-DD: ")
-URL = "https://www.billboard.com/charts/hot-100/"+TIMETRAVEL
-#Spotify
-SPOTIFY_CLIENTID = "48cd0bbcd5f342dc977328f9d3971c1e"
-SPOTIFY_SECRET = "67599e2d234640b8a6d6a4bb23cb8d3a"
-PLAYLIST_ID = "4dzsRiEjzshLcuUQybwHVD"
+TIMETRAVEL = input("which year do you want to travel to? Type in this format: YYYY-MM-DD: ")   
+URL = "https://www.billboard.com/charts/hot-100/"+TIMETRAVEL  #Hot100 Week you will scrape
+#Spotify; remember you have to create an app through Spotify to make it work "https://developer.spotify.com/dashboard/"
+SPOTIFY_CLIENTID = #"Enter your Spotify Client ID"
+SPOTIFY_SECRET = #"Enter your Spotify Secret"
+PLAYLIST_ID = #"Enter your Spotify playlist ID if you just want to update it"
 
 #Scrapping Billboard
 response = requests.get(URL)
-
 billboard_website = response.text
 soup = BeautifulSoup(billboard_website, "html.parser")
 songs = soup.select(selector="li h3", class_ = "c-title")
@@ -24,8 +23,7 @@ billboard_artist_list_plus_extras = [artist.get_text().strip() for artist in art
 
 # Create a new list of songs
 index = billboard_song_list_plus_extras.index("Account")
-billboard_hot100_songs = billboard_song_list_plus_extras[:index]
-print(billboard_hot100_songs)
+billboard_hot100_songs = billboard_song_list_plus_extras[:index] #Your list with the songs
 
 #Create a new list of artists
 billboard_hot100_artists_almost_done = []
@@ -35,7 +33,7 @@ for n in billboard_artist_list_plus_extras:
          billboard_hot100_artists_almost_done.append(n)
  
 billboard_hot100_artists_almost_done = billboard_hot100_artists_almost_done[15:115]
-billboard_hot100_artists = []
+billboard_hot100_artists = []   #Your list with the artirs
   
 for artist in billboard_hot100_artists_almost_done:
     # Split the string at " Featuring " (with spaces)
@@ -45,24 +43,23 @@ for artist in billboard_hot100_artists_almost_done:
   # Append the modified artist to the new list
   billboard_hot100_artists.append(modified_artist)
 
-print(billboard_hot100_artists)
 
 
-#Spotify API
-sp = spotipy.Spotify(
+#Spotify API 
+sp = spotipy.Spotify(    
         auth_manager=SpotifyOAuth(
-            scope = "playlist-modify-public",
-            redirect_uri="http://example.com",
+            scope = "playlist-modify-public", #You can add multiple scopes depending on your goal. check the scopes on the spotofy api documentation https://developer.spotify.com/documentation/web-api/concepts/scopes
+            redirect_uri="http://example.com",   #Needs to be exact the same as you enter when creating the App
             client_id = SPOTIFY_CLIENTID,
             client_secret = SPOTIFY_SECRET,
             show_dialog=True,
             cache_path="token.txt"
         )
-)
-user_id = sp.current_user()["id"]
+) #login, and get the token
+user_id = sp.current_user()["id"] #save UserID for later
 print(user_id)
 
-URIs=[]
+URIs=[]  #You will transfor the songs from the hot100 into URIs to later create and/or update the playlist
 for song in billboard_hot100_songs:
     index= billboard_hot100_songs.index(song)
     result = sp.search(q=f"track:{song} artist:{billboard_hot100_artists[index]}",type="track")
@@ -70,7 +67,7 @@ for song in billboard_hot100_songs:
         uri = result['tracks']['items'][0]['uri']
         URIs.append(uri)
     except:
-        print(f"cannot find {song} by {billboard_hot100_artists[index]} in spotify, skipping")
+        print(f"cannot find {song} by {billboard_hot100_artists[index]} in spotify, skipping") #some of them are not on spotify, other times the name is slightly different in spotify vs billboard
     
     
 pprint.pprint(URIs)
@@ -81,7 +78,7 @@ playlist = sp.user_playlist_create(user=user_id, name=f"{TIMETRAVEL} Hott 100 so
 playlist_id = playlist['id']
 #print(playlist_id)
 
-#once playlist is created... to get back to it is:
+#If your playlist already exists and you just want to get back to it:
 #playlist = sp.playlist(playlist_id)
 
 #Add songs to playlist using playlist id
