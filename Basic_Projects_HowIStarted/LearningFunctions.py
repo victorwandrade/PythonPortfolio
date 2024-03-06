8af5a7f8a4718e8dbe0505d83da617a11f96e912
Victor Andrade <victor.w.andrade@gmail.com>
2024-03-06 10:58:44 -0300

    Update and rename Day 6 - Python Functions & Karel.py to LearningFunctions.py

--- Basic_Projects_HowIStarted/LearningFunctions.py
+++ Basic_Projects_HowIStarted/LearningFunctions.py
@@ -1,6 +1,4 @@
-# Day 6
-#Functions, Karel and While Loops
-
+#my code for https://reeborg.ca/reeborg.html?lang=en&mode=python&menu=worlds%2Fmenus%2Freeborg_intro_en.json&name=Maze&url=worlds%2Ftutorial_en%2Fmaze1.json
 #escaping the maze
 def turn_right():
     turn_left()
